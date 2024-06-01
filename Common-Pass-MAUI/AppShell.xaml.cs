ad3646b791f3b6960a2a0ba08ec814e0037fe18b
common khadka <commonkhadka@gmail.com>
2024-06-01 22:29:28 +0545

    Add project files.

---
+++ Common-Pass-MAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Common_Pass_MAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Common-Pass-MAUI/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Common_Pass_MAUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
